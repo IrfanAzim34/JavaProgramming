9e20d7952f8a0fed328eb21a4417b8d981287e13
Irfan Azim <irfanteipov@gmail.com>
2022-12-15 04:32:30 +0300

    Created new files

---
+++ src/day38_Inheritance/Car.java
@@ -0,0 +1,38 @@
+package day38_Inheritance;
+
+public class Car {
+
+    public String brand,model;
+    public int year;
+    public double price;
+    public String color;
+    public double miles;
+
+    public Car(String brand, String model, int year, double price, String color, double miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
+
+    public void start(){
+        System.out.println("I'm starting " + brand + " " + model);
+    }
+
+    public void drive(){
+        System.out.println("I'm driving " + brand + " " + model);
+    }
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=$" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
+}
---
+++ src/day38_Inheritance/Toyota.java
@@ -0,0 +1,12 @@
+package day38_Inheritance;
+
+public class Toyota extends Car{
+
+    public Toyota(String model, int year, double price, String color, double miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable(){
+        System.out.println(brand + " " + model + " is reliable");
+    }
+}
