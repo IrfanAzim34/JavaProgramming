c63ca2be8b03f8686755b23beb97d993de47a2ca
Irfan Azim <irfanteipov@gmail.com>
2023-02-25 12:49:16 +0300

    Created new files

--- src/day02_HelloWorld/EmployeeInfo.java
+++ src/day02_HelloWorld/EmployeeInfo.java
@@ -3,7 +3,7 @@ package day02_HelloWorld;
 public class EmployeeInfo {
 
     public static void main(String[] args) {
-        System.out.println("your name");
+        System.out.println("Your name");
         System.out.println("your company's name");
         System.out.println("your EmployeeId");
         System.out.println("your JobTitle");
